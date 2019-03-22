aa8da37787e8f9712080f061fb8dbfb56a52b68e
arielzao150 <ariel.molina@gruposplice.com.br>
2019-03-22 17:11:00 -0300

    Reestruturação do projeto.

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Titulo teste</span>
+        <p>Postado por usuário</p>
+        <p className="greay-text">Um dia aí</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary;
