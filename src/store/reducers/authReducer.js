d1220687e4f514b9af05c867a1fb348a9c2f3e0e
Ariel Vitor Molina <arielvmol@terra.com.br>
2019-03-24 16:02:04 -0300

    Sign In.

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,22 @@
-const initState = {}
+const initState = {
+    authError: null
+}
 
 const authReducer = (state = initState, action) => {
-    return state;
+    switch (action.type) {
+        case 'LOGIN_SUCCESS':
+            return {
+                ...state,
+                authError: null
+            }
+        case 'LOGIN_ERROR':
+            return {
+                ...state,
+                authError: 'Erro ao entrar'
+            }
+        default:
+            return state;
+    }
 };
 
 export default authReducer;
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import projectReducer from './projectReducer'
 import cursoReducer from './cursoReducer'
 import { combineReducers } from 'redux'
 import { firestoreReducer } from 'redux-firestore';
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
   curso: cursoReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  firebase: firebaseReducer
 });
 
 export default rootReducer
